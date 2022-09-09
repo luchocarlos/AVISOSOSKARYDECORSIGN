9dc1ff56744d81b8eab088f5673569f633a23214
lusi <islu@MacBook-Pro-de-LUIS.local>
2022-09-09 14:42:28 -0500

    web-v3

---
+++ preload.js
@@ -0,0 +1,15 @@
+newFunction();
+
+function newFunction() {
+    window.onload = setTimeout(function () {
+
+        var preload = document.getElementById('preload');
+        var body = document.getElementById('hidden');
+
+        preload.classList.add('remove');
+        body.classList.remove('hidden');
+    }
+
+    ); 500;
+}
+ 
